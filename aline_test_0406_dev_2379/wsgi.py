829b6182c48aeb33c31daba35a19317080c3b791
Crowdbotics <team@crowdbotics.com>
2020-04-07 18:16:30 +0000

    Update models

--- aline_test_0406_dev_2379/wsgi.py
+++ aline_test_0406_dev_2379/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aline_test_0406_dev_2379.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aline_test_0406_dev_2379.settings")
 
 application = get_wsgi_application()
